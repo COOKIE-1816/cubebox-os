5dd51c2fc093ab697591f98f8e9537bc05671312
COOKIE-1816 <1816.cookie1816@gmail.com>
2023-06-09 18:50:30 +0200

    new file:   .vscode/c_cpp_properties.json 	modified:   build.sh 	new file:   inc/drivers/vga.h 	modified:   src/kernel/kernel.c

--- build.sh
+++ build.sh
@@ -1,1 +1,8 @@
+  src_c=$(find ./src -name *.c)
+#src_asm=$(find ./src -name *.asm)
+
+#nasm -felf32 $src_asm
 nasm -felf32 src/boot.asm -o build/obj/boot.asm.o
+gcc -c $src_c -ffreestanding -O2 -Wall -Wextra -std=gnu99 -I inc
+
+mv *.o build/obj

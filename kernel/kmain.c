3988f89789679a702df7f302a0cf1b1707e97885
COOKIE-1816 <1816.cookie1816@gmail.com>
2023-06-19 13:18:55 +0200

    modified:   kernel/kmain.c

--- kernel/kmain.c
+++ kernel/kmain.c
@@ -60,9 +60,7 @@ enum KEYCODE getKey() {
 void kernel_main(/*multiboot_info* __boot_info*/ void) {
     kernel_initializeKernelParticipals();
 
-    tty_writeString("CubeBox, kernel v0.0.1.0a - Alpha phase.\n");
-    tty_writeString("Copyright (C) Vaclav Hajsman 2023\n");
-    drawLine();
+    tty_writeString("CubeBox, kernel v0.0.1.0a - Alpha phase.\n\n");
 
     pic_init();
 

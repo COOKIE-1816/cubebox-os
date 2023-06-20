31f40d853665ebd958974f9ed9920954e5541888
DidiaWeb3solutions <chrisuser199@gmail.com>
2023-06-20 22:05:05 +0300

    Fix stupid bug I don't remember

--- drivers/acpi/shutdown.c
+++ drivers/acpi/shutdown.c
@@ -1,27 +1,24 @@
 #include "drivers/acpi.h"
 #include "kernel/common.h"
 
+// inline  dword *SMI_CMD = 0;
+// inline  byte ACPI_ENABLE = 0;
+// inline  byte ACPI_DISABLE = 0;
+// inline  dword *PM1a_CNT = 0;
+// inline  dword *PM1b_CNT = 0;
+// inline word SLP_TYPa = 0;
+// inline  word SLP_TYPb = 0;
+// inline  word SLP_EN = 0;
+// inline byte PM1_CNT_LEN = 0;
+// inline word SCI_EN = 0;
 
-inline  dword *SMI_CMD = 0;
-inline  byte ACPI_ENABLE = 0;
-inline  byte ACPI_DISABLE = 0;
-inline  dword *PM1a_CNT = 0;
-inline  dword *PM1b_CNT = 0;
-inline word SLP_TYPa = 0;
-inline  word SLP_TYPb = 0;
-inline  word SLP_EN = 0;
-inline byte PM1_CNT_LEN = 0;
-inline word SCI_EN = 0;
+// void _acpi_ensureEnabled() {
 
-void _acpi_ensureEnabled() {
+//     if(SCI_EN != 0)
+//         acpi_enable();
+// }
 
-    if(SCI_EN != 0)
-        acpi_enable();
-}
-
-
-
- int acpi_shutdown(void) {
+int acpi_shutdown(void) {
     _acpi_ensureEnabled();
         
     outb((unsigned int) PM1a_CNT, SLP_TYPa | SLP_EN );

361a1965e94e7aff81869bf85e721d9b40af669f
COOKIE-1816 <1816.cookie1816@gmail.com>
2023-06-22 16:48:20 +0200

    modified:   drivers/keyboard.h 	modified:   drivers/keyboard.c

--- drivers/keyboard.h
+++ drivers/keyboard.h
@@ -1,11 +1,14 @@
 #ifndef _CB_KBD_H_
 #define _CB_KBD_H_
 
+#include <cboolean.h>
+
 void kbd_setLeds(   bool __n, // NumLock LED
                     bool __c, // CapsLock LED
                     bool __s  // ScrollLock LED
                 );
 
-
+bool kbd_return_isKeypress(uint8_t __return);
+ 
 
 #endif

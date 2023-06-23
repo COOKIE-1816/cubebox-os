e1edd4d96586ba6338e0b130805b4ff6ad498bde
COOKIE-1816 <1816.cookie1816@gmail.com>
2023-06-23 17:45:44 +0200

    modified:   build.sh 	deleted:    kernel/gdt/gdt.asm 	modified:   kernel/gdt/gdt.c 	deleted:    kernel/gdt/gdt.h 	deleted:    kernel/gdt/gdtdef.h 	modified:   kernel/interrupt/idt.c 	deleted:    kernel/interrupt/irq.c 	deleted:    kernel/interrupt/irq.h 	deleted:    kernel/interrupt/isr.asm 	deleted:    kernel/interrupt/isr.c 	modified:   kernel/interrupt/isr.h 	modified:   kernel/tty.h 	kernel/gdt.h 	kernel/gdt/asm.s 	kernel/interrupt/asm/

--- kernel/interrupt/idt.h
+++ kernel/interrupt/idt.h
@@ -1,72 +1,47 @@
-/*
-============================================================= FILE INFORMATION =============================================================
-                 .@@@@@@@@@@@@@@@@@@@@@@@@@@%            
-                 .@@@@@@@@@@@@@@@@@@@@@@@@@@%               Product name:               CubeBox OS
-                 .@@@@@@@@@@@@@@@@@@@@@@@@@@%               Product version:            0.0.1.0a, Alpha stage - unstable
-                 .@@@@@@@@@@@@@@@@@@@@@@@@@@%               
-           @@@@@@@@@@@@@#             %@@@@@@@@@@@@@        File name & path:           /kernel/interrupt/idt.h
-           @@@@@@@@@@@@@#             %@@@@@@@@@@@@@        Programming language:       C (header)
-           @@@@@@@@@@@@@#             %@@@@@@@@@@@@@        File usage:                 Header file for IDT implementation
-           @@@@@@@@@@@@@#             %@@@@@@@@@@@@@                           
-    @@@@@@@@@@@@@&                          ,@@@@@@@        Last revision:              13-06-2023, 15-57 UTC
-    @@@@@@@@@@@@@&                          ,@@@@@@@        Last revision describtion:  
-    @@@@@@@@@@@@@&                          ,@@@@@@@                           
-    @@@@@@@@@@@@@&                                                             
-    @@@@@@@@@@@@@&                                                             
-    @@@@@@@@@@@@@&                                          File usage:                 
-    @@@@@@@@@@@@@&                                          Contributors:               Vaclav Hajsman
-    @@@@@@@@@@@@@&                                                             
-    @@@@@@@@@@@@@&                                                             
-    @@@@@@@@@@@@@&                                          Docs. reference:            
-    @@@@@@@@@@@@@&                                          Online reference:           
-    @@@@@@@@@@@@@&                                                             
-    @@@@@@@@@@@@@&                                                             
-    @@@@@@@@@@@@@&                                          Copyright (C) Vaclav Hajsman (A.K.A. COOKIE) 2023. All rights reserved.
-    @@@@@@@@@@@@@&                                                             
-    @@@@@@@@@@@@@&                                                             
-    @@@@@@@@@@@@@&                                          This file is licensed as a part of the project inself, and licensing information
-    @@@@@@@@@@@@@&                                          Can be found in LICENSE file in root directory of this project.
-============================================================================================================================================
-*/
-
-#ifndef _CB_IDT_H_
-#define _CB_IDT_H_
-
-#define IDT_MAX_DESCRIPTORS 256
+#ifndef _CB_INTERRUPT_IDT_H_
+#define _CB_INTERRUPT_IDT_H_
 
 #include <stdint.h>
-#include "kernel/interrupt/isr.h"
 
-//uint8_t* vectors;
-
-typedef struct {
-	uint16_t    isr_low;
-	uint16_t    kernel_cs;
-	uint8_t     reserved;
-	uint8_t     attributes;
-	uint16_t    isr_high;
-} __attribute__((packed)) idt_entry_t;
-typedef idt_entry_t InterruptDescriptor;
-
-#define IDT_DESCRIPTOR_SIZE sizeof(InterruptDescriptor)
-
-typedef struct {
-	uint16_t	limit;
-	uint32_t	base;
-} __attribute__((packed)) idtr_t;
+#ifndef __E_ARCH_X64
+    typedef struct {
+        uint16_t    isr_low;
+        uint16_t    kernel_cs;
+        uint8_t     reserved;
+        uint8_t     attributes;
+        uint16_t    isr_high;
+    } __attribute__((packed)) idt_entry_t;
+#else
+    typedef struct {
+        uint16_t    isr_low;
+        uint16_t    kernel_cs;
+        uint8_t	    ist;
+        uint8_t     attributes;
+        uint16_t    isr_mid;
+        uint32_t    isr_high;
+        uint32_t    reserved;
+    } __attribute__((packed)) idt_entry_t;
+#endif
 
 __attribute__((aligned(0x10))) 
+static idt_entry_t IDT[256];
 
-//static idt_entry_t idt[256];
+#ifndef __E_ARCH_X64
+    typedef struct {
+        uint16_t	limit;
+        uint32_t	base;
+    } __attribute__((packed)) idtr_t;
+#else
+    typedef struct {
+        uint16_t	limit;
+        uint64_t	base;
+    } __attribute__((packed)) idtr_t;
+#endif
 
-void idt_setDescriptor(uint8_t vector, void* isr, uint8_t flags);
-void idt_init(void);
+static idtr_t IDTR;
 
-typedef void (*InterruptHandler) ();
 
-//InterruptDescriptor interruptVectorTable[IDT_MAX_DESCRIPTORS];
-
-extern  void setVect(uint8_t __vector, InterruptHandler __handler);
-extern void idt_setGate(unsigned char num, unsigned long base, unsigned short sel, unsigned char flags);
+void idt_setDescriptor(uint8_t __vector, void* __isr, uint8_t __flags);
+void idt_init();
 
 #endif

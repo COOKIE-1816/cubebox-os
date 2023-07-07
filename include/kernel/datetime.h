a1bd7ba1445d0fe7c428bb5ac0d9d8ec98427c75
COOKIE-1816 <1816.cookie1816@gmail.com>
2023-07-07 11:49:16 +0200

    modified:   include/kernel/datetime.h

--- include/kernel/datetime.h
+++ include/kernel/datetime.h
@@ -2,11 +2,40 @@
 #define _CB_TIMING_DT_H_
 
 #include <stdint.h>
+#include <stringt.h>
 
 typedef struct {
     uint8_t h;
     uint8_t m;
     uint8_t s;
 } time;
+
+static String months[12] = {
+    "january",
+    "february",
+    "march",
+    "april",
+    "may",
+    "june",
+    "july",
+    "august",
+    "september",
+    "october",
+    "november",
+    "december"
+};
+
+static String months_short[12] = {
+    "jan",
+    "feb",
+    "mar",
+    "jun",
+    "jul",
+    "aug",
+    "sep",
+    "oct",
+    "nov",
+    "dec"
+};
 
 #endif

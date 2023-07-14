b217abda1a14df7ceca3628e5ba5fcc55dab029c
Vaclav Hajsman (a.k.a. COOKIE) <93037688+COOKIE-1816@users.noreply.github.com>
2023-07-14 11:42:00 +0200

    Update kernel.h

--- include/kernel/kernel.h
+++ include/kernel/kernel.h
@@ -1,14 +1,38 @@
 #ifndef _CB_KERNEL_H
 #define _CB_KERNEL_H
 
-#define KERNEL_UNSTABLE
-#define KVERSION "0.0.1.0a"
+#define KERNEL_UNSTABLE // Says that kernel release is unstable
+#define KVERSION "0.0.1.0a" // Defines kernel version string
+
+
 
 struct regs {
-	unsigned int gs, fs, es, ds;
-	unsigned int edi, esi, ebp, esp, ebx, edx, ecx, eax;
-	unsigned int int_no, err_code;
-	unsigned int eip, cs, eflags, useresp, ss;
+/*
+				This struct defines the regs struct
+				containing register data.
+*/
+	unsigned int gs, 
+															fs, 
+															es, 
+															ds;
+															
+	unsigned int edi, 
+															esi, 
+															ebp, 
+															esp, 
+															ebx, 
+															edx, 
+															ecx, 
+															eax;
+															
+	unsigned int int_no, 
+															err_code;
+															
+	unsigned int eip, 
+															cs, 
+															eflags, 
+															useresp, 
+															ss;
 };
 
 #endif

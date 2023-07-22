94920b19935caa66cb791a24efbe50b39c72646e
COOKIE-1816 <1816.cookie1816@gmail.com>
2023-07-22 16:56:01 +0200

    vga

---
+++ src/drivers/vga/vga.cpp
@@ -0,0 +1,25 @@
+#include "drivers/vga/vga.h"
+
+inline void Vga::setPalete(int __bits) {
+    _pallete = __bits;
+}
+
+inline u8 Vga::entryColor1x(enum Vga::color1x __foreground, enum Vga::color1x __background) {
+    return __foreground | __background << 4;
+}
+
+inline u8 Vga::entryColor2x(enum Vga::color2x __foreground, enum Vga::color2x __background) {
+    return __foreground | __background << 4;
+}
+
+inline u8 Vga::entryColor(enum Vga::color4x __foreground, enum Vga::color4x __background) {
+    return __foreground | __background << 4;
+}
+
+inline u16 Vga::entry(uc __char, u8 __color) {
+    return (u16) __char | (u16) __color << 8;
+}
+
+inline void Vga::init() {
+    setPalete(4);
+}

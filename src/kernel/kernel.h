62a9e8d75fc1dc7b24eee46ea02f8164f27e3b19
Vaclav Hajsman (a.k.a. COOKIE) <93037688+COOKIE-1816@users.noreply.github.com>
2023-08-17 16:58:24 +0200

    + version info

--- src/kernel/kernel.h
+++ src/kernel/kernel.h
@@ -1,1 +1,7 @@
+#define KVERSION_STRING "0.0.01.0a"
+#define KDEVSTAGE_STRING "alpha"
+
+const char* kernel_version =  KVERSION_STRING;
+const char* kernel_devStage = KDEVSTAGE_STRING;
+
 #include "kernel/defs/types.h"

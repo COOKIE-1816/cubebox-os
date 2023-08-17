03d0cb158d4f33392164b69e1aa0c03194aa580a
Vaclav Hajsman (a.k.a. COOKIE) <93037688+COOKIE-1816@users.noreply.github.com>
2023-08-17 23:03:29 +0200

    Update kmain.cpp

--- src/kernel/kmain.cpp
+++ src/kernel/kmain.cpp
@@ -1,3 +1,23 @@
+#include "kernel/kernel.h"
+#include "kernel/terminal.h"
+
+void printVersion() {
+    Terminal::wstring("Kernel version: ");
+    Terminal::wstring(kernel_version);
+
+    #if KDEVSTAGE_STRING != "stable"
+        Terminal::wstring(KDEVSTAGE_STRING);
+    #endif
+
+    Terminal::wstring("\n");
+}
+
 extern "C" void kmain(void) {
-    
+    Terminal::init();
+
+    Terminal::setColor(0x02); // Green text
+    Terminal::wstring("CubeBox OS!\n\n");
+
+    Terminal::resetColor();
+    printVersion();
 }

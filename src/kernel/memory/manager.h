08fdd769f99f4145873f8bbdb99fc80cf29656ef
Vaclav Hajsman (a.k.a. COOKIE) <93037688+COOKIE-1816@users.noreply.github.com>
2023-08-18 12:52:47 +0200

    Update manager.h

--- src/kernel/memory/manager.h
+++ src/kernel/memory/manager.h
@@ -1,1 +1,31 @@
+#ifndef MMU_H
 
+#define MMU_BLOCKS_PER_BYTE 8
+#define MMU_BLOCK_SIZE      4096
+#define MMU_BLOCK_ALIGN     MMU_BLOCK_SIZE
+
+#include "kernel/kernel.h"
+
+typedef u32 address_t;
+
+static u32  mmu_memory_size = 0;
+static u33  mmu_usedBlocks  = 0;
+static u32  mmu_maxBlocks   = 0;
+static u32* mmu_memoryMap   = 0;
+
+class MemoryMap {
+    public:
+    static void set(int __bit);
+    static void unset(int __bit);
+    
+    static int test(int __bit);
+    
+    static int firstFree();
+};
+
+static void mmu_init(size_t __size, address_t __bitmap);
+static void mmu_initRegion(address_t __base, size_t __regionSize);
+static void mmu_uninitRegion(address_t __base, size_t __regionSize);
+
+static void free(void *__p);
+static void malloc();

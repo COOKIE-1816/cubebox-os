278fe6d3b2a7140fc053d2894d2779227ac226a9
Vaclav Hajsman (a.k.a. COOKIE) <93037688+COOKIE-1816@users.noreply.github.com>
2023-08-18 12:00:22 +0200

    Update size.s

--- src/kernel/memory/size.s
+++ src/kernel/memory/size.s
@@ -1,1 +1,19 @@
+memory_size:
+    mov ax, 0x88
 
+	  int 0x15
+	  jc .error
+
+	  test ax, ax
+	  je .error
+
+	  cmp ah, 0x86
+	  je .error
+
+	  cmp ah, 0x80
+	  je .error
+
+	  ret
+.error:
+	  mov	ax, -1
+	  ret

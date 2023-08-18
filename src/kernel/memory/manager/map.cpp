cc522f3880e5d92057d5556a9a187eee227d2968
Vaclav Hajsman (a.k.a. COOKIE) <93037688+COOKIE-1816@users.noreply.github.com>
2023-08-18 13:16:38 +0200

    Update map.cpp

--- src/kernel/memory/manager/map.cpp
+++ src/kernel/memory/manager/map.cpp
@@ -1,1 +1,9 @@
+#include "kernel/memory.h"
 
+inline void MemoryMap::set(int __bit) {
+    mmu_memoryMap[__bit / 32] |= (1 << (__bit % 32));
+}
+
+inline void MemoryMap::unset(int __bit) {
+    mmu_memoryMap[__bit / 32] &= ~ (1 << (__bit % 32));
+}

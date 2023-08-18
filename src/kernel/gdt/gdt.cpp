c94df27420230fb367942830bcc23e707f6c8bca
Vaclav Hajsman (a.k.a. COOKIE) <93037688+COOKIE-1816@users.noreply.github.com>
2023-08-18 11:01:52 +0200

    Update gdt.cpp

--- src/kernel/gdt/gdt.cpp
+++ src/kernel/gdt/gdt.cpp
@@ -1,1 +1,18 @@
+#include "kernel/gdt.h"
+#include <stdio.h>
 
+u64 Gdt::createDescriptor(gdt_descriptor_template_t __descriptor) {
+    u64 descriptor;
+ 
+    descriptor  =  limit       & 0x000F0000;         // set limit bits 19:16
+    descriptor |= (flag <<  8) & 0x00F0FF00;         // set type, p, dpl, s, g, d/b, l and avl fields
+    descriptor |= (base >> 16) & 0x000000FF;         // set base bits 23:16
+    descriptor |=  base        & 0xFF000000;         // set base bits 31:24
+ 
+    descriptor <<= 32;
+ 
+    descriptor |= base  << 16;                       // set base bits 15:0
+    descriptor |= limit  & 0x0000FFFF;               // set limit bits 15:0
+ 
+    printf("0x%.16llX\n", descriptor);
+}

f4aa32d1394528c0fac81f6f518b5203ce4692ca
Vaclav Hajsman (a.k.a. COOKIE) <93037688+COOKIE-1816@users.noreply.github.com>
2023-08-18 11:31:31 +0200

    Update gdt.cpp

--- src/kernel/gdt/gdt.cpp
+++ src/kernel/gdt/gdt.cpp
@@ -16,3 +16,23 @@ u64 Gdt::createDescriptor(gdt_descriptor_template_t __descriptor) {
  
     printf("0x%.16llX\n", descriptor);
 }
+
+void Gdt::encodeEntry(u8* __target, gdt_descriptor_template_t __source) {
+    if (__source.limit > 0xFFFFF) {
+        printf("Gdt: error: GDT entry too large (> 0x000fffff).");
+        return;
+    }
+ 
+    __target[0] = __source.limit & 0xFF;
+    __target[1] = (__source.limit >> 8) & 0xFF;
+    __target[6] = (__source.limit >> 16) & 0x0F;
+ 
+    __target[2] = __source.base & 0xFF;
+    __target[3] = (__source.base >> 8) & 0xFF;
+    __target[4] = (__source.base >> 16) & 0xFF;
+    __target[7] = (__source.base >> 24) & 0xFF;
+ 
+    __target[5] = __source.access_byte;
+ 
+    __target[6] |= (__source.flags << 4);
+}

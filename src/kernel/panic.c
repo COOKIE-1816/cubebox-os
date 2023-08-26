2db226c3f099d4e05b6f1b7ce93da4ea1acf2a16
Vaclav Hajsman (a.k.a. COOKIE) <93037688+COOKIE-1816@users.noreply.github.com>
2023-08-26 17:46:42 +0200

    Save panic.c.

--- src/kernel/panic.c
+++ src/kernel/panic.c
@@ -1,1 +1,12 @@
+#include "kernel/panic.h"
+#include "kernel/tty.h"
 
+void panic(string __error) {
+     // disable interrupts
+    __asm__ volatile ("cli");
+    
+    Terminal::Cursor::disable();
+    
+    Terminal::wstring("\n\n\n");
+    Terminal::wstring("======= [ KERNEL PANIC ] =======");
+}

227aa4495ef72906f31a4f61f6aad7573ee97190
Vaclav Hajsman (a.k.a. COOKIE) <93037688+COOKIE-1816@users.noreply.github.com>
2023-08-26 12:35:24 +0200

    Save panic.h.

--- src/kernel/panic.h
+++ src/kernel/panic.h
@@ -1,1 +1,17 @@
+#ifndef _PANIC_H_
+#define _PANIC_H_
 
+#include "string.h"
+#include "kernel/kernel.h"
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+void panic(string __error);
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif

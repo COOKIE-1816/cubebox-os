353945039fc26fbaa894c8420c306567ee55308b
COOKIE-1816 <1816.cookie1816@gmail.com>
2023-09-22 08:46:07 +0200

    Fixed some things accidentaly missed

--- src/kernel/kmsg.cpp
+++ src/kernel/kmsg.cpp
@@ -2,14 +2,14 @@
 #include "terminal.h"
 //#include "vga.h"
 
-static String* kmsg_str[6] {
+const char* kmsg_str[6] {
 	"WARN",
 	"ERROR",
 	"***",
 	"INFO",
 	"NOTE",
 	"FATAL"
-}
+};
 
 static u8 kmsg_colors[6] {
 	0b00000101,	// Brown
@@ -18,7 +18,7 @@ static u8 kmsg_colors[6] {
 	0b00000110, // Default
 	0b00000110, // Default 
 	0b10001100, // Blinking light red
-}
+};
 
 /*String kmsgGen(int __level, String __source, String __message) {
 	
@@ -26,7 +26,7 @@ static u8 kmsg_colors[6] {
 
 void kmsg(int __level, String __source, String __message) {
 	Terminal::setColor(kmsg_colors[__level]);
-	Terminal::wstring(kmsg_str);
+	Terminal::wstring(kmsg_str[__level]);
 	Terminal::wstring("\t");
 	
 	Terminal::resetColor();

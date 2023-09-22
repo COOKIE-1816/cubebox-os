60e0cbe80b557453df8e12e35bc27923fb2177aa
COOKIE-1816 <1816.cookie1816@gmail.com>
2023-09-22 08:12:23 +0200

    Fix search path bug

--- build.sh
+++ build.sh
@@ -4,14 +4,16 @@ SOURCES_ASM=$(find -type f -name "*.s")
 SOURCES_CPP=$(find -type f -name "*.cpp")
 SOURCES_C=$(find -type f -name "*.c")
 SOURCES_H=$(find -type f -name "*.h")
-CFLAGS=" -ffreestanding -B src -B src/libs/libc -O2 -Wall -fno-exceptions -Wextra "
+WORKDIR=$PWD
+CFLAGS=" -ffreestanding -O2 -Wall -fno-exceptions -Wextra "
+SEARCH=" -Isrc -Isrc/libs/libc "
 echo "-> Assembling assembly language sources..." 
 i686-elf-as $SOURCES_ASM
 echo "-> Compiling source code..." 
 echo "   C   | $SOURCES_C" 
-i686-elf-gcc $CFLAGS  -c -lc $SOURCES_C  -std=gnu99  
+i686-elf-gcc $CFLAGS  -c -lc $SOURCES_C $SEARCH -std=gnu99  
 echo "   C++ | $SOURCES_C" 
-i686-elf-g++ $CFLAGS  -c -lc $SOURCES_CPP -fno-rtti   
+i686-elf-g++ $CFLAGS  -c -lc $SOURCES_CPP $SEARCH -fno-rtti   
 echo "-> Linking object files together..." 
 i686-elf-g++ -T src/linker.ld -o build/bin/cubebox.bin -ffreestanding -O2 -nostdlib build/obj/* -lgcc  
 OBJFILES=$(find -type f -name "*.o")

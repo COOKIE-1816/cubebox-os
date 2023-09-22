805e9ba9aff4df31f2627b34a0190bbd84445acd
COOKIE-1816 <1816.cookie1816@gmail.com>
2023-09-22 09:00:48 +0200

    Fixed extra params

--- src/libs/libc/stdlib/abort.cpp
+++ src/libs/libc/stdlib/abort.cpp
@@ -3,11 +3,13 @@
  
 __attribute__((__noreturn__))
 void abort(void) {
-#if defined(__is_libk)
-	printf("kernel: panic: abort()\n");
+	#if defined(__is_libk)
+		printf("kernel: panic: abort()\n");
         asm volatile("hlt");
-#else	printf("abort()\n");
-#endif
+	#else 
+		printf("abort()\n");
+	#endif
+	
 	while(1);
 	__builtin_unreachable();
 }

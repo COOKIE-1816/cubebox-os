4462cba0cb13b2a525108f4e7796ad702265508d
Vaclav Hajsman (a.k.a. COOKIE) <1816cookie.1816@gmail.com>
2023-09-27 17:05:07 +0200

    GDT

---
+++ src/kernel/gdt/gdt.s
@@ -0,0 +1,35 @@
+.section text
+
+gdt_start:
+    dd 0x0
+    dd 0x0
+
+gdt_code:
+    # Segment length
+    dw 0xffff
+
+    # Segment base
+    dw 0x0
+    db 0x0
+
+    db 10011010b ; flags (8 bits)
+    db 11001111b ; flags (4 bits) + segment length
+
+    db 0x0
+
+gdt_data:
+    dw 0xffff
+
+    dw 0x0
+    db 0x0
+
+    db 10010010b
+    db 11001111b
+
+    db 0x0
+
+gdt_end:
+
+gdt_descriptor:
+    dw gdt_end - gdt_start - 1
+    dd gdt_start

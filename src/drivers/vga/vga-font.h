865e6ef61de4ad3a441364578d82d0470114b023
Vaclav Hajsman (a.k.a. COOKIE) <1816cookie.1816@gmail.com>
2023-09-28 09:22:16 +0200

    bugfix

---
+++ src/drivers/vga/vga-font.h
@@ -0,0 +1,8 @@
+#ifndef VGA_FONT_H
+#define VGA_FONT_H
+
+/*
+    16x9
+*/
+
+#endif
---
+++ src/kernel/kernel/kernel.c
@@ -0,0 +1,4 @@
+#include "kernel.h"
+
+const String kernel_version =  (char*) "0.01.01.0a";
+const String kernel_devStage = (char*) "alpha";

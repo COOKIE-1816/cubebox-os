71a13d88a9923fe153b08c978f680ca3fd9a8e2e
Vaclav Hajsman (a.k.a. COOKIE) <1816cookie.1816@gmail.com>
2023-09-28 09:12:50 +0200

    panic bugfix

--- src/kernel/panic.c
+++ src/kernel/panic.c
@@ -1,5 +1,6 @@
 #include "kernel/panic.h"
 #include "kernel/terminal.h"
+#include "drivers/vga/vga.h"
 
 void __halt() {
     terminal_wstring("System halted.");
@@ -16,8 +17,14 @@ void panic(String __error) {
     
     terminal_cursor_disable();
     
-    terminal_wstring("\n\n\n");
-    terminal_wstring("======= [ KERNEL PANIC ] =======\n");
+    terminal_wstring("\n");
+    terminal_wstring("======= [ KERNEL PANIC ] ");
+
+    for(int i = 0; i < VGA_WIDTH - 25; i++)
+        terminal_wstring("=");
+
+    // No need to make a line break, since it is already
+    // made by printing those "equal to" marks.
     
     terminal_setColor(0b10001100);
     terminal_wstring("- kernel panic: ");
@@ -31,6 +38,7 @@ void panic(String __error) {
 }
 
 void asmpanic() {
+    terminal_wstring("End of kernel initialization routines ( kmain() ).\n");
 	panic("KERNEL_PANIC_UNK");
 }
 
